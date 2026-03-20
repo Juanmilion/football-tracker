fd3fb6d1d7a10d04bf977edb68bf034d42d484dc
Juanma <juanmaguerrerx@gmail.com>
2026-03-20 11:56:57 +0100

    Floating btn change

--- src/components/FloatingButton.tsx
+++ src/components/FloatingButton.tsx
@@ -4,7 +4,7 @@ import { FaPlus } from "react-icons/fa"
 export default function FloatingButton({ onClick }: any) {
     return (
         <button className="fab" onClick={onClick}>
-            <FaPlus />
+            +
         </button>
     )
 }
