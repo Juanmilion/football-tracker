ef99e9621b8899e1e356374da0a54603830dab90
Juanma <juanmaguerrerx@gmail.com>
2026-04-04 23:20:20 +0200

    add spped insights and analitycs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { BrowserRouter } from "react-router-dom"
 import Layout from "./layouts/Layout"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Analytics />
+      <SpeedInsights />
       <Layout />
     </BrowserRouter>
   )
